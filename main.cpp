bb108cafd500162cc50b788a6c864a567aa88557
jiri133 <jiri133@users.noreply.github.com>
2025-03-10 17:30:52 +0200

    fishies

--- main.cpp
+++ main.cpp
@@ -1,57 +1,67 @@
 #include <iostream>
-#include <array>
+#include <vector>
 
-#include <Helper.h>
+class Fish
+{
+ std :: string name;
+ int size;
+ int speed;
+ int growthFactor;
 
-int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
-    return 0;
+public:
+ Fish(const std :: string& name, int size, int speed, int growthFactor = 1)//constrde initializare
+ : name(name), size(size), speed(speed), growthFactor(growthFactor){}
+
+ Fish ( const Fish& f )//const de copiere
+ :name(f.name),size(f.size),speed(f.size),growthFactor(f.growthFactor){}
+
+
+ //operator=
+ Fish& operator=(const Fish& other) {
+  if (this != &other) {  // Verificăm auto-atribuierea
+   name = other.name;
+   size = other.size;
+   speed = other.speed;
+   growthFactor = other.growthFactor;
+  }
+  return *this;  // Returnăm obiectul curent
+ }
+ void applyReward(int bonus) {
+  size += bonus;
+  speed += bonus / 2;
+  std::cout << name << " a primit un bonus! Dimensiune: " << size << ", Viteză: " << speed << std::endl;
+ }
+
+
+ void display() const
+ {
+  std :: cout << name << " " << size << " " << speed << " " << growthFactor << std :: endl;
+ }
+};
+class Rewards
+{
+ std :: string type;
+ int value;
+public:
+ Rewards(const std :: string& type, int value = 1)
+     : type(type), value(value){}
+ Rewards(const Rewards& r)
+     :type(r.type),value(r.value){}
+
+
+
+
+};
+
+class Aquarium
+{
+ std :: vector <Fish> fishies;
+ std :: vector <Rewards> rewards;
+};
+
+int main()
+{
+ Fish nemo("Nemo",20,3,1);
+ nemo.display();
+ return 0;
 }
